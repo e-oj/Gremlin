839d991fc7622dc2da7bd59377e1aec9c1b82162
emmanuel <eolaojo@yahoo.com>
2017-12-23 19:33:48 -0500

    Refactored

--- app/api/user/index.js
+++ app/api/user/index.js
@@ -6,13 +6,10 @@
 let express = require("express");
 
 let user = require("./u");
-// let auth = require("../utils/authToken");
 
 let userRouter = express.Router();
 
-userRouter.route("/")
-  .post(user.createUser);
-
+userRouter.post("/", user.createUser);
 userRouter.post("/login", user.login);
 
 module.exports = userRouter;
