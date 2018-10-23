115ca953e93c590f3d1a302fc6149796a46f6c7f
emmanuel <eolaojo@yahoo.com>
2018-10-23 12:14:39 -0400

    responsive styles

--- public/src/components/site/home.vue
+++ public/src/components/site/home.vue
@@ -12,8 +12,10 @@
                               target="_blank">@e-oj</a>)
         </div>
 
-        <div class="splash-text">
-          {{text}}
+        <div class="splash">
+          <div class="splash-text" v-for="trait in traits" :key="trait">
+            {{trait}}
+          </div>
         </div>
 
         <div class="links">
@@ -40,36 +42,41 @@
 </template>
 
 <script>
-  import Nav from "./nav.vue"
+import Nav from "./nav.vue";
 
-  const DEFAULT_TEXT = "I'm a full-stack developer and Computer " +
+const DEFAULT_TEXT = "I'm a full-stack developer and Computer " +
     "Science major at Rochester Institute of Technology. Check out " +
     "my work on Github, follow me on Medium or connect on LinkedIn.";
 
-  export default{
-    data(){
-      return {
-        text: ""
-      }
-    },
+export default{
+  data(){
+    return {
+      text: "",
+      traits: [
+        "Full-stack Developer",
+        "Entrepreneur",
+        "World Peace"
+      ]
+    };
+  },
 
-    components: {
-      "nav-bar": Nav
-    },
+  components: {
+    "nav-bar": Nav
+  },
 
-    async created(){
-      let self = this;
+  async created(){
+    let self = this;
 
-      try{
-        let res = await self.$http.get("/api/h");
+    try{
+      let res = await self.$http.get("/api/h");
 
-        self.text = res.body.result.home.text;
-      }
-      catch(err){
-        self.text = DEFAULT_TEXT;
-      }
+      self.text = res.body.result.home.text;
+    }
+    catch(err){
+      self.text = DEFAULT_TEXT;
     }
   }
+};
 </script>
 
 <style scoped>
@@ -93,18 +100,22 @@
   .content-wrapper{
     width: 100%;
     height: 70vh;
+    min-height: fit-content;
     display: flex;
     justify-content: center;
     align-items: center;
+  }
+
+  .content, .oj-image{
+    height: 600px;
   }
 
   .content{
     display: flex;
     flex-direction: column;
     justify-content: space-between;
-    height: 600px;
     min-height: 450px;
-    margin-right: 25px;
+    margin-right: 50px;
   }
 
   .content div{
@@ -124,15 +135,24 @@
     font-family: Acme, cursive;
   }
 
+  .content .splash{
+    display: flex;
+    flex-direction: column;
+    flex-shrink: 0;
+    justify-content: space-evenly;
+    height: 200px;
+  }
+
   .content .splash-text{
-    width: 500px;
-    min-width: 450px;
+    min-width: 300px;
+    max-width: 300px;
     padding: 15px;
     font-family: Quicksand, sans-serif;
     font-size: 18px;
     font-weight: bold;
     line-height: 2;
     color: #24292e;
+    background-color: #f2f2f2;
     box-shadow: 0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24);
   }
 
@@ -157,11 +177,10 @@
 
   .oj-image{
     width: fit-content;
-    height: 600px;
     min-height: 450px;
     display: flex;
     align-items: center;
-    margin-left: 25px;
+    margin-left: 50px;
   }
 
   .oj-image img{
@@ -179,4 +198,40 @@
   .medium-logo{
     color: #24292e;
   }
+
+  @media screen and (max-width: 1500px){
+    .home{
+      min-height: 400px;
+    }
+
+    .content, .oj-image{
+      height: 400px;
+      min-height: 350px;
+    }
+
+    .content .intro{
+      font-size: 25px;
+      padding: 10px;
+    }
+
+    .content .splash{
+      height: 200px;
+    }
+
+    .content .splash-text{
+      font-size: 14px;
+      padding: 10px;
+    }
+
+    .content .links a{
+      height: 30px;
+      padding: 10px;
+    }
+
+    .content .links a svg{
+      width: 30px;
+      height: 30px;
+    }
+  }
+
 </style>
