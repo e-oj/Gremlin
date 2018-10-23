479a54662a840b835643d8cea48ff58ee6305506
e-oj <eoo5498@rit.edu>
2018-10-23 02:38:10 -0400

    better homepage

--- public/src/components/site/home.vue
+++ public/src/components/site/home.vue
@@ -1,29 +1,39 @@
 <template>
   <div class="home">
-    <nav-bar></nav-bar>
+    <nav-bar is-fixed="true"></nav-bar>
 
-    <div class="content">
-      <div class="intro">
-        Emmanuel Olaojo (<a href="https://github.com/e-oj"
-                            target="_blank">@e-oj</a>)
+    <div class="nav-ghost"></div>
+
+    <div class="content-wrapper">
+      <div class="content">
+
+        <div class="intro">
+          Emmanuel Olaojo (<a href="https://github.com/e-oj"
+                              target="_blank">@e-oj</a>)
+        </div>
+
+        <div class="splash-text">
+          {{text}}
+        </div>
+
+        <div class="links">
+          <a href="https://github.com/e-oj">
+            <i class="fab fa-github github-logo"></i>
+          </a>
+
+          <a href="https://www.linkedin.com/in/emmanuel-olaojo/">
+            <i class="fab fa-linkedin linkedin-logo"></i>
+          </a>
+
+          <a href="https://medium.com/@emmanuelolaojo">
+            <i class="fab fa-medium medium-logo"></i>
+          </a>
+        </div>
+
       </div>
 
-      <div class="splash-text">
-        {{text}}
-      </div>
-
-      <div class="links">
-        <a href="https://github.com/e-oj">
-          <i class="fab fa-github"></i>
-        </a>
-
-        <a href="https://www.linkedin.com/in/emmanuel-olaojo/">
-          <i class="fab fa-linkedin"></i>
-        </a>
-
-        <a href="https://medium.com/@emmanuelolaojo">
-          <i class="fab fa-medium"></i>
-        </a>
+      <div class="oj-image">
+        <img src="/src/assets/img/oj.png" onerror="this.style.display = 'none'">
       </div>
     </div>
   </div>
@@ -62,70 +72,111 @@
   }
 </script>
 
-<style>
+<style scoped>
+  .nav-ghost{
+    height: 100px;
+    flex-shrink: 0;
+    opacity: 0;
+  }
+
   .home{
-    position: absolute;
     width: 100%;
-    height: 100%;
-    color: #e6f1ff;
-    background-color: #0a192f;
+    max-width: 1920px;
+    height: 100vh;
+    min-height: 600px;
+    margin: auto;
     display: flex;
     flex-direction: column;
-    justify-content: center;
-  }
-
-  .home .content{
-    margin: 0 auto;
-    display: flex;
-    flex-direction: column;
-    width: 80%;
-    height: 70%;
-    justify-content: space-evenly;
-    font-family: 'Amaranth', sans-serif;
-  }
-
-  .home .splash-text{
-    width: 46%;
-    padding: 20px;
-    max-width: 700px;
-    font-family: 'Source Code Pro', sans-serif;
-    line-height: 1.4;
     background-color: white;
-    color: #24292e;
-    font-size: 1em;
   }
 
-  .home .intro{
+  .content-wrapper{
+    width: 100%;
+    height: 70vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .content{
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    height: 600px;
+    min-height: 450px;
+    margin-right: 25px;
+  }
+
+  .content div{
+    background-color: white;
+  }
+
+  .content .intro{
+    width: fit-content;
+    padding: 15px;
+    font-family: "Signika", cursive;
+    font-size: 30px;
     font-weight: bold;
-    font-size: 1.4em;
+    color: #24292e;
   }
 
-  .home .intro a{
-    text-decoration: none;
-    border-bottom: 5px solid #ffff00;
-    transition: all 0.2s linear;
+  .content .intro a{
+    font-family: Acme, cursive;
   }
 
-  .home .intro a:hover{
-    color: #ffff00
+  .content .splash-text{
+    width: 500px;
+    min-width: 450px;
+    padding: 15px;
+    font-family: Quicksand, sans-serif;
+    font-size: 18px;
+    font-weight: bold;
+    line-height: 2;
+    color: #24292e;
+    box-shadow: 0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24);
   }
 
-  .home .links{
-    font-size: 1.3em;
+  .content .links{
+    width: 250px;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
   }
 
-  .home .links a{
-    text-decoration: none;
-    color: #42b983;
-    transition: all 0.2s linear;
-    margin-right: 35px;
+  .content .links a{
+    flex-shrink: 0;
+    width: fit-content;
+    height: 35px;
+    padding: 15px;
   }
 
-  .home .links a:last-child{
-    margin-right: 0;
+  .content .links a svg{
+    width: 35px;
+    height: 35px;
   }
 
-  .home .links a:hover{
-    color: #ffff00;
+  .oj-image{
+    width: fit-content;
+    height: 600px;
+    min-height: 450px;
+    display: flex;
+    align-items: center;
+    margin-left: 25px;
+  }
+
+  .oj-image img{
+    height: 100%;
+  }
+
+  .github-logo{
+    color: #24292e;
+  }
+
+  .linkedin-logo{
+    color: #0177B5;
+  }
+
+  .medium-logo{
+    color: #24292e;
   }
 </style>
